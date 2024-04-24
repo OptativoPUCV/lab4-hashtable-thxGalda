e861e4a66b7e587318c5d8ae9bd8f9f181e0f754
Amaro Fibla <163011598+thxGalda@users.noreply.github.com>
2024-04-24 18:44:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -113,7 +113,8 @@ void eraseMap(HashMap * map,  char * key)
 Pair * searchMap(HashMap * map,  char * key) 
 {   
   int pos = get_valid_location(map, key);
-  if (map->buckets[pos]->key == key){
+  if (map->buckets[pos] != NULL && strcmp(map->buckets[pos]->key, key))
+  {
     return map->buckets[pos];
   }
   else
