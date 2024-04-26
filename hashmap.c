a228187cde849945813bb0f15d569bd13fba6504
Amaro Fibla <163011598+thxGalda@users.noreply.github.com>
2024-04-26 18:48:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -113,7 +113,6 @@ void eraseMap(HashMap * map,  char * key)
     if (is_equal(map->buckets[pos]->key, key))
     {
       map->buckets[pos]->key = NULL;
-      //map->buckets[pos]->value = NULL;
       map->size--;
       return;
     }
@@ -138,8 +137,16 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
-
-    return NULL;
+  map->current = -1;
+  for (int i = 0; i < map->capacity; i++)
+    {
+      if (map->buckets[i] != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
