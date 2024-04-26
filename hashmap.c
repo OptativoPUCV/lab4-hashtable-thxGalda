991214fd7c5ec5e3e20b7b3087c693b60657f3ad
Amaro Fibla <163011598+thxGalda@users.noreply.github.com>
2024-04-26 19:29:14 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -139,7 +139,7 @@ Pair * firstMap(HashMap * map)
 {
   for (int i = 0; i < map->capacity; i++)
     {
-      if (map->buckets[i] != NULL)
+      if (map->buckets[i]->key != NULL)
       {
         map->current = i;
         return map->buckets[i];
